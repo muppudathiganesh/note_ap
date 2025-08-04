94099a3655c7059e86325a995bdeeb5424625194
muppudathiganesh <muppuraj11@gmail.com>
2025-08-04 18:25:55 +0530

    Add files via upload

---
+++ notes/views.py
@@ -0,0 +1,55 @@
+
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Note
+from .forms import NoteForm, RegisterForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('note_list')
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+@login_required
+def note_list(request):
+    notes = Note.objects.filter(user=request.user)
+    return render(request, 'notes/note_list.html', {'notes': notes})
+
+@login_required
+def note_create(request):
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            note = form.save(commit=False)
+            note.user = request.user
+            note.save()
+            return redirect('note_list')
+    else:
+        form = NoteForm()
+    return render(request, 'notes/note_form.html', {'form': form})
+
+@login_required
+def note_update(request, pk):
+    note = get_object_or_404(Note, pk=pk, user=request.user)
+    form = NoteForm(request.POST or None, instance=note)
+    if form.is_valid():
+        form.save()
+        return redirect('note_list')
+    return render(request, 'notes/note_form.html', {'form': form})
+
+@login_required
+def note_delete(request, pk):
+    note = get_object_or_404(Note, pk=pk, user=request.user)
+    if request.method == 'POST':
+        note.delete()
+        return redirect('note_list')
+    return render(request, 'notes/note_confirm_delete.html', {'note': note})
+
+def home_view(request):
+    return render(request, 'home.html')
